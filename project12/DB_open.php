c7e4076f8ba617e1311a896a303f8f7bde4cf890
ou891226-spec <ou891226@gmail.com>
2025-07-23 13:44:55 +0800

    Update DB_open.php

--- project12/DB_open.php
+++ project12/DB_open.php
@@ -6,17 +6,16 @@ $host = 'smartfun-senior.mysql.database.azure.com';
 $db = 'myproject';
 $user = 's1411131021';
 $pass = 'Test12345'; // 你的密碼
-$ssl_ca = __DIR__ . '/BaltimoreCyberTrustRoot.crt.pem';
 
 $options = [
-    PDO::MYSQL_ATTR_SSL_CA => $ssl_ca,
     PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+    PDO::MYSQL_ATTR_SSL_CA => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass, $options);
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4;sslmode=require", $user, $pass, $options);
 } catch (PDOException $e) {
     die("連線失敗：" . $e->getMessage());
 }
