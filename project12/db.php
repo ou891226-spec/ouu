71305f75ee65626e192e831243c54bd7050ffe9b
ou891226-spec <ou891226@gmail.com>
2025-07-23 13:45:40 +0800

    Update db.php

--- project12/db.php
+++ project12/db.php
@@ -1,22 +1,19 @@
 <?php
 $host = 'smartfun-senior.mysql.database.azure.com';
-$user = 's1411131021@smartfun-senior';
-$pass = 'Test12345'; // 變數名要和下面一致
-$dbname = 'myproject'; // 你的資料庫名稱
-$ssl_ca = __DIR__ . '/BaltimoreCyberTrustRoot.crt.pem';
-
-if (!file_exists($ssl_ca)) {
-    die("SSL 憑證不存在於: $ssl_ca");
-}
+$user = 's1411131021';
+$pass = 'Test12345'; 
+$dbname = 'myproject'; 
 
 try {
   $pdo = new PDO(
-    "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+    "mysql:host=$host;dbname=$dbname;charset=utf8mb4;sslmode=require",
     $user,
     $pass,
     [
-      PDO::MYSQL_ATTR_SSL_CA => $ssl_ca,
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+      PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+      PDO::MYSQL_ATTR_SSL_CA => false,
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]
   );
 } catch (PDOException $e) {
